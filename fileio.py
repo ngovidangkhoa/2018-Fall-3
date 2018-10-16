220b07ad917b29f64394a6f6dfe0942644f5b53e
ms pais <42386796+mspais@users.noreply.github.com>
2018-10-16 09:16:23 -0400

    Update fileio.py

--- fileio.py
+++ fileio.py
@@ -1,3 +1,18 @@
 fobject = open('important.txt','w')
 fobject.write('go go go')
-fob.close()
+fobject.close()
+
+fobject = open('important.txt','r')
+fobject.read(5)
+fobject.read()
+fobject.close()
+
+fobject = open('important.txt','r')
+print fobject.readline()
+print fobject.readlines()
+fobject.close()
+
+fobject = open('important.txt','r')
+fobject.write('line1\nline2\nline3\nline4')
+fobject.close()
+
